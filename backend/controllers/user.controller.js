695cfc29fadd2df8b80ab63b1e39d0ad57f0c130
Musfiraa <musfiraaarif12@gmail.com>
2025-05-26 10:34:22 +0300

    created userupdate api

--- backend/controllers/user.controller.js
+++ backend/controllers/user.controller.js
@@ -1,2 +1,28 @@
 import bcrypt from "bcryptjs";
-import User from "./models/User.js";
+import User from "../models/User.js";
+import { errorHandler } from "../utils/error.js";
+export const updateUSER = async (req, res, next) => {
+  if (req.user.id !== req.params.id)
+    return next(errorHandler(403, "You can only update your own profile!"));
+  try {
+    if (req.body.password) {
+      req.body.password = bcrypt.hashSync(req.body.password, 10);
+    }
+    const updatedUser = await User.findByIdAndUpdate(
+      req.params.id,
+      {
+        $set: {
+          username: req.body.username,
+          email: req.body.email,
+          password: req.body.password,
+          avatar: req.body.avatar,
+        },
+      },
+      { new: true }
+    );
+    const { password, ...rest } = updatedUser._doc;
+    res.status(200).json(rest);
+  } catch (error) {
+    next(error);
+  }
+};
--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,4 +1,13 @@
 import express from "express";
-import {verifyToken} from 
+import { verifyToken } from "../utils/verifyUser.js";
+import { updateUSER } from "../controllers/user.controller.js";
 const router = express.Router();
-router.get("/:id",)
+//why did we use verifyToken here?
+// The verifyToken middleware is used to ensure that the user is authenticated before allowing them to update their profile.    
+// can u explain me the post request in the context of this route?
+// In this context, the POST request to "/update/:id" is used to update a user's profile information. 
+// The ":id" parameter in the URL represents the unique identifier of the user whose profile is being updated. 
+// The verifyToken middleware checks if the user is authenticated before proceeding with the update operation.
+router.post("/update/:id",verifyToken,updateUSER);
+
+export default router;
