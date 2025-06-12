489c93461befe890361f4b7e2b8fe6b7c9a9924b
Musfiraa Arif <140048632+Mooosiee@users.noreply.github.com>
2025-06-13 00:33:39 +0300

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -10,7 +10,10 @@ import submissionRouter from "./routes/submission.route.js";
 import customInputRouter from "./routes/customIn.route.js"
 dotenv.config();
 const app = express();
-app.use(cors()); 
+app.use(cors(
+    origin: "https://og-oj-frontend.onrender.com",
+    credentials: true, 
+)); 
 app.use(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({extended:true}));
