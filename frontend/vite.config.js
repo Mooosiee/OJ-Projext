24e59c5f7637735377340236030287466fb95ca1
Musfiraa Arif <140048632+Mooosiee@users.noreply.github.com>
2025-06-13 00:44:49 +0300

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,12 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    proxy: {
-      "/backend": {
-        target: "https://og-oj-backend.onrender.com",
-        secure: false,
-      },
-    },
   },
 });
