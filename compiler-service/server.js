413f16438fbba9718654bfafd9c44fcb55ceedd1
Musfiraa Arif <140048632+Mooosiee@users.noreply.github.com>
2025-06-13 15:15:19 +0300

    Update server.js

--- compiler-service/server.js
+++ compiler-service/server.js
@@ -5,16 +5,18 @@ import {startScheduledCleanup} from "./scheduler.js"
 const app = express();
 
 app.use(express.json());
-app.use("/compiler",compilerRouter);
+
 app.use(cors({
   origin: 'https://og-oj-backend.onrender.com' // Only allow main backend
 }));
 
+app.use("/compiler",compilerRouter);
 
-
-
-app.listen(8000,() => {
-    console.log("Server is running on PORT 8000");
+//Listen on correct PORT for Render
+//Render doesn’t expose custom ports like 8000.
+const PORT = process.env.PORT || 8000;
+app.listen(PORT,() => {
+    console.log(`Server is running on PORT ${PORT}`);
 });
  // --- Start the scheduled cleanup tasks : After server starts listening---
   startScheduledCleanup(); // <--- CALL THE FUNCTION
