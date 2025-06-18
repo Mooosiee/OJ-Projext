d9650658c1a05f5bb88a724d9b2ce7158d2f0086
Musfiraa <musfiraaarif12@gmail.com>
2025-06-18 19:16:30 +0300

    PORT edit

--- backend/server.js
+++ backend/server.js
@@ -38,8 +38,8 @@ app.use((err,req,res,next) =>{
     });
 })
 
-
-app.listen(process.env.PORT,() => {
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,() => {
    console.log(`Server is listening on port ${process.env.PORT}!`);
    
 });
